44cc0d09a5009c3280b7066366a464191cbc1059
Pablo Amorós Becerra <pab23@gcloud.ua.es>
2019-03-21 14:07:34 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$("#draggable").draggable();
+	$( "#dropable" ).droppable({
+      drop: function( event, ui ) {
+        $( this )
+          .addClass( "ui-state-highlight" )
+          .find( "p" )
+            .html( "Dropped!" );
+      }
+    });
+});
