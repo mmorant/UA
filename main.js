241f6427fbee31b75dc4bcc970ff1b1bd8226f04
Pablo Amorós Becerra <pab23@gcloud.ua.es>
2019-04-16 12:45:33 +0200

    añadido lvl2

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+	var lvl = -1;
+	//LVL1
 	var cuadrado = new Object();
 	cuadrado.imagen = "./resources/cuadrado.png";
 	cuadrado.nombre = "cuadrado";
@@ -11,48 +13,71 @@ $(document).ready(function(){
 	var rombo = new Object();
 	rombo.imagen = "./resources/rombo.png";
 	rombo.nombre = "rombo";
+	//LVL2
+	var estrella = new Object();
+	estrella.imagen = "./resources/estrella.png";
+	estrella.nombre = "estrella";
+	var ovalo = new Object();
+	ovalo.imagen = "./resources/ovalo.png";
+	ovalo.nombre = "ovalo";
+	var pentagono = new Object();
+	pentagono.imagen = "./resources/pentagono.png";
+	pentagono.nombre = "pentagono";
+	var hexagono = new Object();
+	hexagono.imagen = "./resources/hexagono.png";
+	hexagono.nombre = "hexagono";
 
-	var formas = [cuadrado, triangulo, circulo, rombo];
+	var formas = [[cuadrado, triangulo, circulo, rombo],[estrella, ovalo, pentagono, hexagono]];
 	var rnd = Math.floor(Math.random()*4);
-	formas = formas.sort(function() {return Math.random() - 0.5});
+	
+	
+	nextLvl();
+	
 
-	formas.forEach(function(el){
-		$(".item-container").append("<div class='draggable "+el.nombre+"'><img src='"+el.imagen+"' alt='' width='100' height='100'></div>");
-	});
-	$(".forma").html(formas[rnd].nombre);
+    function nextLvl(){
+    	lvl++;
+    	$(".item-container").html("");
+    	formas[lvl] = formas[lvl].sort(function() {return Math.random() - 0.5});
+    	for( let i = 0; i < formas[lvl].length; i++){
+			console.log(formas[lvl][i]);
+			$(".item-container").append("<div class='draggable "+formas[lvl][i].nombre+"'><img src='"+formas[lvl][i].imagen+"' alt='' width='100'></div>");
+		}
+		console.log(formas);
+		$(".forma").html(formas[lvl][rnd].nombre);
 
-	$(".draggable").each(function(){
-		$(this).draggable({
-			revert: "invalid",
-			cursor: "move",
-			containment: $(this).parent().parent()
-		});
-		console.log($(this).parent().parent());
-	})
-	$( "#dropable" ).droppable({
-		//accept: "."+formas[rnd].nombre,
-     	drop: function( event, ui ) {
-     		if(ui.draggable.hasClass(formas[rnd].nombre)){	  
-			    $( "#dialog-confirm" ).dialog({
-			      resizable: false,
-			      height: "auto",
-			      width: 400,
-			      modal: true,
-			      buttons: {
-			        "Siguiente nivel": function() {
-			          $( this ).dialog( "close" );
-			        },
-			        Salir: function() {
-			          $( this ).dialog( "close" );
-			        }
+		$(".draggable").each(function(){
+			$(this).draggable({
+				revert: "invalid",
+				cursor: "move",
+				containment: $(this).parent().parent()
+			});
+		})
+		$( "#dropable" ).droppable({
+			//accept: "."+formas[rnd].nombre,
+	     	drop: function( event, ui ) {
+	     		if(ui.draggable.hasClass(formas[lvl][rnd].nombre)){	  
+				    $( "#dialog-confirm" ).dialog({
+				      resizable: false,
+				      height: "auto",
+				      width: 400,
+				      modal: true,
+				      buttons: {
+				        "Siguiente nivel": function() {
+				          $( this ).dialog( "close" );
+				          nextLvl();
+				        },
+				        Salir: function() {
+				          $( this ).dialog( "close" );
+				        }
 			      }
 			  	}).prev(".ui-dialog-titlebar").css("color","green");;
-     		}
-     		else{
-     			$(ui.draggable).draggable({ revert: true});
-     			$("#modalError").show();
-     		}
-     	}
-    });
+	     		}
+	     		else{
+	     			$(ui.draggable).draggable({ revert: true});
+	     			$("#modalError").show();
+	     		}
+	     	}
+	    });
+    }
 });
 
